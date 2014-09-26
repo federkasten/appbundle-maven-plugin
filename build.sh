ec1ed7397d89e8f6fc4a11b7ed197bd63f68b794
Takashi AOKI <federkasten@me.com>
2014-09-26 10:53:51 +0900

    cosmetic change

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-OS=`sw_vers  -productVersion | cut -f 1,2 -d "." `
+VERSION=`sw_vers -productVersion | cut -f 1,2 -d "."`
 CC=clang
-if [[ -z "$JAVA_HOME" ]]; then 
-	JAVA_HOME=`/usr/libexec/java_home`
+
+if [[ -z "$JAVA_HOME" ]]; then
+    JAVA_HOME=`/usr/libexec/java_home`
 fi
+
 INCLUDE="-I ${JAVA_HOME}/include -I ${JAVA_HOME}/include/darwin"
 FRAMEWORK="-framework Cocoa"
 SRC="native/main.m"
 DST="src/main/resources/io/github/appbundler/JavaAppLauncher"
-SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX$OS.sdk"
+SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${VERSION}.sdk"
 ARCH="x86_64"
 MIN_MAC_OSX_VERSION="10.7"
 
